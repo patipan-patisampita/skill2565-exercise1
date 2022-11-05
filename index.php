20e888774c111dfe7d6be3428124e284e3e7c216
patipan-patisampita <patipan.patisampita@gmail.com>
2022-11-05 14:55:25 +0700

    update

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercise1</title>
+
+    <!--1.Bootstrap CSS-->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h1>Hello World!</h1>
+            </div>
+
+            <div class="card-body">
+                <h1>Body</h1>
+            </div>
+        </div>
+    </div>
+
+    <!--2.Bootstrap JS-->
+    <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
