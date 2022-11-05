bb3d8d06db099fcc3340678efeb10bb10687641a
patipan-patisampita <patipan.patisampita@gmail.com>
2022-11-05 15:59:42 +0700

    update

---
+++ login.php
@@ -0,0 +1,16 @@
+<?php include("inc/header.php"); ?>
+<?php include("inc/navbar.php"); ?>
+
+<div class="container my-2">
+    <div class="card">
+        <div class="card-header">
+            <h1>เข้าสู่ระบบ</h1>
+        </div>
+
+        <div class="card-body">
+            <h1>Body Login Page</h1>
+        </div>
+    </div>
+</div>
+
+<?php include("inc/footer.php"); ?>
---
+++ register.php
@@ -0,0 +1,16 @@
+<?php include("inc/header.php"); ?>
+<?php include("inc/navbar.php"); ?>
+
+<div class="container my-2">
+    <div class="card">
+        <div class="card-header">
+            <h1>ลงทะเบียน</h1>
+        </div>
+
+        <div class="card-body">
+            <h1>Body Regsiter Page</h1>
+        </div>
+    </div>
+</div>
+
+<?php include("inc/footer.php"); ?>
